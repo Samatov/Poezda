bdd03fee90b0043da444e92d2079ad68d9a70dd5
Сергей Саматов < >
2015-11-06 12:05:48 +0300

    отмечается последняя остановка

---
+++ MMT_v.2.1/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace MMT_v._2._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(7, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(121, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Установить время";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            this.checkBox1.Click += new System.EventHandler(this.checkBox1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(134, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(32, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(178, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(32, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(131, -1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Часы";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(175, -1);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Минуты";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(216, 12);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(120, 20);
+            this.dateTimePicker1.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(7, 36);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1280, 720);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(342, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 30000;
+           
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1362, 741);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ MMT_v.2.1/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MMT_v._2._1
+{
+    public partial class Form1 : Form
+    {
+        int hourInt; int i = 0, j, count = 0;
+        int minInt;
+        int secInt;
+        int X, Y;
+        string hour, min, sec, realtime;
+        bool stop = true;
+        int timeBegginHour = 08, timeBegginMin = 00, timeEndHour = 16, timeEndMin = 00;
+        ListBox list = null;
+        PictureBox pictureBoxs = null;
+        string[] ArrTrain = { "427", "429" };
+        int[] coordinates = new int[] { 50, 48, 107, 48, 177, 48, 244, 48, 337,48,420,48,505,48,596,688,781,940};
+        //string[] timeStops = new string[] { "08:30", "09:00", "09:30", "10:00" };
+        string[] timeStops = new string[] { "08", "00", "08", "23", "08", "50", "09", "20", "11", "45", "13", "30","14","05", "14", "15", "16", "00" };
+        
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+          
+
+        /*   if (hourInt >= timeBegginHour)
+           {
+               if (hourInt > timeBegginHour)
+               {
+                   MessageBox.Show(" выехал");
+                   getcoordinate();
+                   draw();
+               }
+               if (hourInt == timeBegginHour)
+               {
+                   if (minInt >= timeBegginMin)
+                   {
+                       MessageBox.Show(" выехал");
+                       getcoordinate();
+                       draw();
+                   }
+                   else
+                       MessageBox.Show("Не выехал");
+
+
+               }
+           }
+           else
+               MessageBox.Show("Не выехал");*/
+
+          // timer1.Start(); 
+            
+
+        }
+
+
+
+        
+        void proverka()
+        {
+
+        }
+
+        void getcoordinate()
+        {
+            getTime();
+            int i = 0;
+            count = 0;
+            string stops1, stops2;
+            int stops1Int, stops2Int;
+            string realtime = hour + min;
+            int realtimeInt = int.Parse(realtime);
+
+
+
+            int lenght = timeStops.Length;
+            while ((stop))
+            {
+                stops1 = timeStops[i] + timeStops[i + 1];
+                stops2 = timeStops[i + 2] + timeStops[i + 3];
+                stops1Int = int.Parse(stops1);
+                stops2Int = int.Parse(stops2);
+                if ((realtimeInt >= stops1Int) & (realtimeInt < stops2Int))
+                {
+                    stop = false;
+                }
+                if ((realtimeInt >= stops1Int) & (realtimeInt >= stops2Int))
+                {
+                    i = i + 2;
+                    count = count + 2;
+                    stop = true;
+                }
+
+
+
+
+            }
+            stop = false;
+            X = coordinates[count];
+            Y = coordinates[count + 1];
+
+        }
+
+        void draw()
+        {
+            if (pictureBoxs != null)
+            {
+                pictureBox1.Controls.Remove(pictureBoxs);
+                pictureBoxs = null;
+            }
+            pictureBoxs = new PictureBox();
+            pictureBoxs.Size = new Size(22, 22);
+            
+            // pictureBox1.Refresh();
+            pictureBoxs.Load("seat.jpg");
+           
+            pictureBoxs.BackColor = Color.Transparent;
+            pictureBoxs.Location = new Point(X,Y);
+            // i = i + 2;
+            pictureBox1.Controls.Add(pictureBoxs);
+        }
+
+        void getTime()
+        {
+            
+            hour = DateTime.Now.ToString("HH");
+            min = DateTime.Now.ToString("mm");
+            sec = DateTime.Now.ToString("ss");
+            textBox1.Text = hour;
+            textBox2.Text = min;
+
+           // string realtime = DateTime.Now.ToString("HH:mm");
+            hourInt = int.Parse(hour);
+            minInt = int.Parse(min);
+            secInt = int.Parse(sec);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+           // getTime();
+            proverka();
+            getcoordinate();
+            draw();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
+        }
+
+        private void checkBox1_Click(object sender, EventArgs e)
+        {
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
+        }
+
+       
+        
+        
+
+       
+
+
+       
+    }
+    
+ 
+
+    
+}
